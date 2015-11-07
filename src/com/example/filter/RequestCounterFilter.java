b07db5296c470d023a0d746fc2ff922ef69071d0
semihokanpehlivan <semihokanp@gmail.com>
2015-11-07 16:42:40 +0300

    Implementing RequestCounterFilter.doFilter()

--- src/com/example/filter/RequestCounterFilter.java
+++ src/com/example/filter/RequestCounterFilter.java
@@ -3,6 +3,7 @@ package com.example.filter;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * @author Semih Okan Pehlivan.
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class RequestCounterFilter implements Filter{
 
     private FilterConfig filterConfig;
+    private static Logger logger = Logger.getLogger(RequestCounterFilter.class.getName());
 
     public void init(FilterConfig filterConfig) throws ServletException {
 //        set-up when the container instantiate the this filter
@@ -20,6 +22,8 @@ public class RequestCounterFilter implements Filter{
 //        TODO: Business logic of the filter
 //        the parameters is not HTTPSevletRequest or Response, it need to be casted to HTTPServets
         HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println(req);
+        logger.info("Request info:"+ req.getLocalPort());
     }
 
     public void destroy() {
